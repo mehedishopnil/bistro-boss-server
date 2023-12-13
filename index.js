fa26785706174b9fd5d96415717fb726a07826a6
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-12-13 08:48:41 +0600

    new update

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) =>{
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+
+        //bearer token
+    const token = authorization.split(' '[1]); 
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) =>{
+        if(error){
+            return res.status(401). send({error: true, message: 'unauthorized access'})
+        }
+
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 console.log();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -34,6 +53,14 @@ async function run() {
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewsCollection = client.db("bistroDB").collection("reviews");
         const cartCollection = client.db("bistroDB").collection("cart");
+
+
+        //jwt token::
+        app.post ('/jwt', (req, res)=>{
+            const user = req.body;
+            const token = jwt.sign (user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send(token);
+        })
 
 
         //users related apis
@@ -82,11 +109,17 @@ async function run() {
         })
 
         //Cart Collection apis::
-        app.get('/cart', async (req, res) => {
+        app.get('/cart', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([])
             }
+
+            const decodedEmail = req.decoded.email;
+            if(email !== decodedEmail){
+                return res.status(403).send({error: true, message: 'porviden access'})
+            }
+
             const query = { email: email };
             const result = await cartCollection.find(query).toArray();
             res.send(result);
