ab2d846bdd2c463c7a05936f626a78e0e406275c
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-12-30 16:27:20 +0600

    menu delete operation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const  jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,18 +9,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const verifyJWT = (req, res, next) =>{
+const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
-    if(!authorization){
-        return res.status(401).send({error: true, message: 'unauthorized access'})
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
     }
 
-        //bearer token
-    const token = authorization.split(' ')[1]; 
-    
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-        if(err){
-            return res.status(401).send({error: true, message: 'unauthorized access'})
+    //bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
         }
         req.decoded = decoded;
         next();
@@ -55,20 +55,20 @@ async function run() {
 
 
         //jwt token::
-        app.post ('/jwt', (req, res)=>{
+        app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-            res.send({token});
+            res.send({ token });
         })
 
         //Middleware::
         //Warning: use verifyJWT before using verifyAdmin.
-        const verifyAdmin = async(req,res, next) =>{
+        const verifyAdmin = async (req, res, next) => {
             const email = req.decoded.email;
-            const query = {email:email};
+            const query = { email: email };
             const user = await usersCollection.findOne(query);
-            if(user?.role !== 'admin'){
-                return res.status(403).send({error:true, message: 'forbidden message'});
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
             }
             next();
         }
@@ -80,17 +80,17 @@ async function run() {
         */
 
         //users related apis
-        app.get('/users',verifyJWT, verifyAdmin, async(req, res)=> {
-            const result =await usersCollection.find().toArray();
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
+            const result = await usersCollection.find().toArray();
             res.send(result);
         })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = {email: user.email};
+            const query = { email: user.email };
             const existingUser = await usersCollection.findOne(query);
-            if(existingUser){
-                return res.send({message:'user already exist'})
+            if (existingUser) {
+                return res.send({ message: 'user already exist' })
             }
             const result = await usersCollection.insertOne(user);
             res.send(result);
@@ -100,27 +100,27 @@ async function run() {
         //email same
         //check admin
 
-        app.get('/users/admin/:email',verifyJWT, async(req, res) => {
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
 
-            if(req.decoded.email !== email) {
-                res.send({admin: false})
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
             }
-            const query = {email: email};
+            const query = { email: email };
             const user = await usersCollection.findOne(query);
-            const result = {admin: user?.role === 'admin'}
+            const result = { admin: user?.role === 'admin' }
             res.send(result);
         })
 
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: {
-                  role: 'admin',
+                    role: 'admin',
                 },
-              };
+            };
 
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.send(result);
@@ -134,11 +134,19 @@ async function run() {
             res.send(result);
         })
 
-        app.post('/menu', verifyJWT, verifyAdmin, async(req, res)=>{
+        app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
             const newItem = req.body;
             const result = await menuCollection.insertOne(newItem);
             res.send(result);
         })
+
+        app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        });
+
 
         //review related apis
         app.get('/reviews', async (req, res) => {
@@ -147,15 +155,15 @@ async function run() {
         })
 
         //Cart Collection apis::
-        app.get('/cart',verifyJWT, async(req, res) => {
+        app.get('/cart', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([])
             }
 
             const decodedEmail = req.decoded.email;
-            if(email !== decodedEmail){
-                return res.status(403).send({error: true, message: 'porviden access'})
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'porviden access' })
             }
 
             const query = { email: email };
